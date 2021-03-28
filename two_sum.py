228b7e6e41012980a690471dd48806c863e22dd5
blackantywj <ywjysys520@outlook.com>
2021-03-28 20:04:31 +0800

    first commit

---
+++ two_sum.py
@@ -0,0 +1,18 @@
+def twoSum(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    h = {}
+    for i, num in enumerate(nums):
+        n = target - num
+        if n not in h:
+            h[num] = i
+        else:
+            return [h[n], i]
+
+nums = [2, 7, 11, 14]
+target = 9
+
+twoSum(nums, target)
