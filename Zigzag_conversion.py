345384bfad763d1f832d1f4210fd787680857eec
blackantywj <ywjysys520@outlook.com>
2021-03-31 22:12:08 +0800

    2021/03/31

---
+++ Zigzag_conversion.py
@@ -0,0 +1,15 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        dic = []
+        corrent = numRows - 1
+        list = range(corrent+1) + range(corrent).reverse()
+
+
+            
+
+        print(result)
+teststring = 'PAYPALISHIRING'
+
+cc = Solution()
+
+cc.convert(teststring, 3)
