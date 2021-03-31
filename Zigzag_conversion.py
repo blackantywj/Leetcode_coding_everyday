97228a0747e015a1830685439b26b1e46fb41838
blackantywj <ywjysys520@outlook.com>
2021-03-31 22:14:10 +0800

    2021/03/31

--- Zigzag_conversion.py
+++ Zigzag_conversion.py
@@ -1,15 +1,37 @@
-class Solution:
-    def convert(self, s: str, numRows: int) -> str:
-        dic = []
-        corrent = numRows - 1
-        list = range(corrent+1) + range(corrent).reverse()
+# class Solution:
+#     def convert(self, s: str, numRows: int) -> str:
+#         dic = []
+#         corrent = numRows - 1
+#         list = range(corrent+1) + range(corrent).reverse
 
 
             
 
-        print(result)
-teststring = 'PAYPALISHIRING'
+#         print(result)
+# teststring = 'PAYPALISHIRING'
 
-cc = Solution()
+# cc = Solution()
 
-cc.convert(teststring, 3)
+# cc.convert(teststring, 3)
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        L = [''] * numRows
+        index, step = 0, 1
+
+        for x in s:
+            L[index] += x
+            if index == 0:
+                step = 1
+            elif index == numRows -1:
+                step = -1
+            index += step
+
+        return ''.join(L)
