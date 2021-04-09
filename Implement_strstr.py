9ce841975862106b27447a5a65bfa0dde22a9f9a
blackantywj <ywjysys520@outlook.com>
2021-04-09 22:11:10 +0800

    2021/04/09

--- Implement_strstr.py
+++ Implement_strstr.py
@@ -14,7 +14,12 @@ class Solution:
             else:
                 continue
         return -1
-        
+    # def strStr(self, haystack: str, needle: str) -> int:
+    #     if len(needle) == 0: return 0
+    #     for i in range(0, len(haystack)-len(needle)+1):
+    #         if haystack[i:i+len(needle)] == needle:
+    #             return i
+    #     return -1
 cc = Solution()
 
 print(cc.strStr('a', ''))
