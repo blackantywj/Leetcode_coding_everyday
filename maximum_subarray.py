6dca054d9c319b85a52943b4158d713b699b3819
blackantywj <ywjysys520@outlook.com>
2021-04-11 22:16:20 +0800

    2021/04/11

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -4,3 +4,18 @@ class Solution:
                 if nums[i-1] > 0:
                     nums[i] += nums[i-1]
         return max(nums)
+
+class Solution:
+    # @param A, a list of integers
+    # @return an integer
+    # 6:57
+    def maxSubArray(self, A):
+        if not A:
+            return 0
+
+        curSum = maxSum = A[0]
+        for num in A[1:]:
+            curSum = max(num, curSum + num)
+            maxSum = max(maxSum, curSum)
+
+        return maxSum    
